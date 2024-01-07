d7abf4fdc98f4bcd83bdfa203720451b34b64b11
Michael Peralta <peralta.michael27@gmail.com>
2024-01-07 04:29:34 -0500

    2- create car that are 20px high by 40 wide that are randomly generated along the y-axis and move to the left edge og ther screen, no cars should be generate in the top and bottom 50px of the screen. Hint: generate a new car only every 6th time the game loop runs.
    3- Detect when the turtle player collides with  a car stop the game if this happnes.
    
    4- Detect when the turtle has reached the top edge ofg the screen( i.e., reached the finish_line_y). when this happens, return the turtle to the statting position and incrase the spped of the cars.
    
    Hint Think about crating and attribute and using the move_increment to incrase the car speed,
    
    5-  Create a scoreboard that keeps track of which level the users is on. every time the turtle player does a successful crossing, the level should increase. when the turtle hits a car, game over should be displayed in the center.

--- main.py
+++ main.py
@@ -4,18 +4,39 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-player_1 = Player()
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
-
+player_1 = Player()
+cars_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player_1.move, "Up")
 
+n_cars = 0
 game_is_on = True
 while game_is_on:
+    n_cars += 1
     time.sleep(0.1)
     screen.update()
+
+    cars_manager.create_car()
+    # move the cars
+    cars_manager.move()
+
+    for car in cars_manager.all_cars:
+        # Detect collision with cars
+        if car.distance(player_1) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+
+    # Detect when Turtle has reached the top edge of the screen
+    if player_1.is_at_the_finish_line():
+        player_1.go_to_start()
+        scoreboard.increase_level()
+        cars_manager.increase_speed()
+
 
 screen.exitonclick()
