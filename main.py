70816533a2ae09f199dc920e9d91afdb8f7f2e1a
Michael Peralta <peralta.michael27@gmail.com>
2024-01-07 02:40:38 -0500

    Create Turtle

--- main.py
+++ main.py
@@ -4,11 +4,18 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+player_1 = Player()
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+
+screen.listen()
+screen.onkey(player_1.move, "Up")
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+screen.exitonclick()
--- player.py
+++ player.py
@@ -7,6 +7,13 @@ FINISH_LINE_Y = 280
 class Player(Turtle):
     def __init__(self):
         super().__init__()
+        self.shape("turtle")
+        self.penup()
+        self.goto(STARTING_POSITION)
+        self.setheading(90)
 
+    def move(self):
+        new_ycor = self.ycor() + MOVE_DISTANCE
+        self.goto(self.xcor(), new_ycor)
 
 
